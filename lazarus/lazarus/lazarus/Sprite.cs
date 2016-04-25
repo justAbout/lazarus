6f1763e238e7d0de46646402b7fb5ccc80d50ff5
Felix Luenebach <felix_luenebach@gmx.de>
2016-04-25 20:59:15 +0200

    enhanced sprite class

--- lazarus/lazarus/lazarus/Sprite.cs
+++ lazarus/lazarus/lazarus/Sprite.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace lazarus
 {
     class Sprite
     {
+        public Vector2 Position { get; set; }
+        public Texture2D Texture { get; set; }
+        public SpriteBatch SpriteBatch { get; set; }
+
+
+        public Sprite(Texture2D texture, Vector2 position, SpriteBatch batch)
+        {
+            Texture = texture;
+            Position = position;
+            SpriteBatch = batch;
+        }
+        public virtual void Draw()
+        {
+            SpriteBatch.Draw(Texture, Position, Color.White);
+        }
+        public Rectangle Bounds
+        {
+            get { return new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height); }
+        }
     }
 }
